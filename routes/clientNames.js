d62a2ad79e12679e1a01496fa00120127939a5b2
ShabiGardezi <shabigardezi51214@gmail.com>
2023-10-27 16:56:48 -0500

    local seo form updated

--- routes/clientNames.js
+++ routes/clientNames.js
@@ -1,21 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const Client = require("../schemas/clients"); // Replace with your actual Ticket model
+const Client = require("../schemas/clients");
 
-// router.get("/client-names", async (req, res) => {
-//   try {
-//     const search = req.query.search || ""; // Get the search query from the request
-//     // Fetch client names from your Ticket collection that match the search query
-//     const clientNames = await Ticket.distinct("businessdetails.clientName", {
-//       "businessdetails.clientName": { $regex: search, $options: "i" },
-//     });
-
-//     res.json({ clientNames });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Error fetching client name suggestions" });
-//   }
-// });
 // Endpoint for client name suggestions
 router.get("/suggestions", async (req, res) => {
   const { query } = req.query; // Get the user's input
