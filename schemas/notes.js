779d7923bdbe4bf0c7139c3fa9e5835892270d45
ShabiGardezi <shabigardezi51214@gmail.com>
2023-12-05 10:42:13 -0600

    notes

--- schemas/notes.js
+++ schemas/notes.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
+// Function to format date as yy-MM-DD
+const formatDate = (date) => {
+  const year = date.getFullYear().toString();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  return `${year}-${month}-${day}`;
+};
+
 const NotesSchema = new mongoose.Schema({
   user_id: {
     type: mongoose.Schema.Types.ObjectId,
@@ -10,5 +18,20 @@ const NotesSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  status: {
+    type: Boolean,
+    default: true,
+  },
+  date: {
+    type: String, // Save the date as a string
+    default: formatDate(new Date()), // Set the default value to the current formatted date
+  },
 });
+
+// Pre-save hook to format the date before saving
+NotesSchema.pre("save", function (next) {
+  this.date = formatDate(new Date());
+  next();
+});
+
 module.exports = mongoose.model("note", NotesSchema);
