dea9c10a94328ecd99662849b0ada34d070eae33
Hamza <hkkh1441@gmail.com>
2023-12-05 13:35:46 +0500

    Active and Complete notes issues fixed

--- routes/notes.js
+++ routes/notes.js
@@ -2,73 +2,92 @@ const router = require("express").Router();
 const Notes = require("../schemas/notes");
 
 router.get("/", async (req, res) => {
-  // get notes
-  try {
-    const { userId } = req.query;
-    const notes = await Notes.find({ user_id: userId });
+    // get notes
+    try {
+        const {userId} = req.query;
+        const notes = await Notes.find({user_id: userId});
 
-    return res.status(200).json({ payload: notes, message: "notes fetched" });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ payload: "", message: "something went wrong" });
-  }
+        return res.status(200).json({payload: notes, message: "notes fetched"});
+    } catch (error) {
+        return res
+            .status(500)
+            .json({payload: "", message: "something went wrong"});
+    }
 });
 
 router.post("/", async (req, res) => {
-  // create a new note
-  try {
-    const { userId, note } = req.body;
-    const newNote = new Notes({
-      user_id: userId,
-      note: note,
-    });
-    const savedNote = await newNote.save();
-    if (!savedNote)
-      return res
-        .status(500)
-        .json({ payload: "", message: "something went wrong" });
+    // create a new note
+    try {
+        const {userId, note, status} = req.body;
+        const newNote = new Notes({
+            user_id: userId,
+            note: note,
+            status: status,
+        });
+        const savedNote = await newNote.save();
+        if (!savedNote)
+            return res
+                .status(500)
+                .json({payload: "", message: "something went wrong"});
 
-    return res.status(200).json({ payload: savedNote, message: "note saved" });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ payload: "", message: "something went wrong" });
-  }
+        return res.status(200).json({payload: savedNote, message: "note saved"});
+    } catch (error) {
+        return res
+            .status(500)
+            .json({payload: "", message: "something went wrong"});
+    }
 });
 
+router.put("/update", async (req, res) => {
+    const { id,status } = req.body;
+    console.log('Received noteID:', id);
+    try {
+        const updatedNote = await Notes.findByIdAndUpdate(
+            id,
+            { status },
+            { new: true } // Return the updated document
+        );
+        if (!updatedNote) {
+            return res.status(404).json({ error: "Note not found" });
+        }
+        res.json(updatedNote);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
 router.delete("/", async (req, res) => {
-  // delete a  note
-  try {
-    const { noteId } = req.query;
-    const deletedNote = await Notes.findByIdAndDelete(noteId);
-    if (!deletedNote)
-      return res
-        .status(500)
-        .json({ payload: "", message: "something went wrong" });
+    // delete a  note
+    try {
+        const {noteId} = req.query;
+        const deletedNote = await Notes.findByIdAndDelete(noteId);
+        if (!deletedNote)
+            return res
+                .status(500)
+                .json({payload: "", message: "something went wrong"});
 
-    return res
-      .status(200)
-      .json({ payload: deletedNote, message: "note deleted" });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ payload: "", message: "something went wrong" });
-  }
+        return res
+            .status(200)
+            .json({payload: deletedNote, message: "note deleted"});
+    } catch (error) {
+        return res
+            .status(500)
+            .json({payload: "", message: "something went wrong"});
+    }
 });
 
 router.delete("/all", async (req, res) => {
-  // delete all notes of a user
-  try {
-    const { userId } = req.query;
-    await Notes.deleteMany({ user_id: userId });
+    // delete all notes of a user
+    try {
+        const {userId} = req.query;
+        await Notes.deleteMany({user_id: userId});
 
-    return res.status(200).json({ payload: "", message: "notes deleted" });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ payload: "", message: "something went wrong" });
-  }
+        return res.status(200).json({payload: "", message: "notes deleted"});
+    } catch (error) {
+        return res
+            .status(500)
+            .json({payload: "", message: "something went wrong"});
+    }
 });
 
 module.exports = router;
--- schemas/notes.js
+++ schemas/notes.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
 
 const NotesSchema = new mongoose.Schema({
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  note: {
-    type: String,
-    required: true,
-  },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    note: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: Boolean,
+        default: true,
+    }
 });
 module.exports = mongoose.model("note", NotesSchema);
