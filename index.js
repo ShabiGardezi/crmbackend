f93eecb6e51177cad4a0e9452048a76e424985f2
ShabiGardezi <shabigardezi51214@gmail.com>
2024-01-03 11:25:33 -0600

    work status item header

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "*", // Allow requests from this origin
-    methods: "GET, POST, PUT, DELETE", // Allowed HTTP methods
+    methods: "GET, POST, PUT, PATCH, DELETE", // Allowed HTTP methods
   })
 );
 
